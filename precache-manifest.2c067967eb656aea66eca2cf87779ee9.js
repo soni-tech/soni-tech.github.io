f34ba5de9dadd8f64bdf6b70321c6d9ad3cc529c
soni-tech <sksspst@gmail.com>
2020-03-06 15:55:52 +0700

    deploy

---
+++ precache-manifest.2c067967eb656aea66eca2cf87779ee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5f92359506068f638e76d8e575f798",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53e9c7acc531ad4834ad",
+    "url": "/static/css/main.01470288.chunk.css"
+  },
+  {
+    "revision": "4a989b91f3c7c1daba1f",
+    "url": "/static/js/2.e6f0e37a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e6f0e37a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e9c7acc531ad4834ad",
+    "url": "/static/js/main.af30e81d.chunk.js"
+  },
+  {
+    "revision": "44dc0a73a7f334cda480",
+    "url": "/static/js/runtime-main.5c75de6b.js"
+  }
+]);
